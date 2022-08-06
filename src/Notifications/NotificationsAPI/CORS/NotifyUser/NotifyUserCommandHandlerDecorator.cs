7e003b7ff4ebd27a37b46d453cc14b44828bea61
EvertonKluczkovski <kluczkovski@hotmail.com>
2022-08-06 18:32:30 +0100

    Implementation of Decorate

---
+++ src/Notifications/NotificationsAPI/CORS/NotifyUser/NotifyUserCommandHandlerDecorator.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using Newtonsoft.Json;
+using NotificationsAPI.Infrastructure.Services;
+using Serilog;
+
+namespace NotificationsAPI.CORS.NotifyUser
+{
+    public class NotifyUserCommandHandlerDecorator : ICommandHandler<NotifyUserCommand, Task>
+    {
+        private readonly NotifyUserCommandHandler _handler;
+
+        public NotifyUserCommandHandlerDecorator(NotifyUserCommandHandler handler)
+        {
+            _handler = handler;
+        }
+
+        public async Task Handler(NotifyUserCommand command)
+        {
+            Log.Information($"Command {command.GetType().Name} was handled with data {JsonConvert.SerializeObject(command)}");
+
+            await _handler.Handler(command);
+        }
+    }
+}
+
